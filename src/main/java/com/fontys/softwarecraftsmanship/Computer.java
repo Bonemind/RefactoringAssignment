ccc5116a6ffc9d4e1f5f6936b895b78eec7bc640
Bonemind <Subhime@gmail.com>
2016-04-13 21:19:11 +0200

    Stuff

--- src/main/java/com/fontys/softwarecraftsmanship/Computer.java
+++ src/main/java/com/fontys/softwarecraftsmanship/Computer.java
@@ -32,27 +32,19 @@ public class Computer {
             Part part = parts.get(i);
             price.add(part.GetPrice());
         }
-        
         return price.getValue();
     }
     
     public boolean IsComplete() {
         ClassList presentClasses = new ClassList();
-        // Build a list of partclasses present
         for (Part part : parts) {
             presentClasses.add(part.getClass());
         }
 
         ClassList requiredClasses = new ClassList();
 
-        // Build a list of partclasses required
         requiredClasses.addAll(Arrays.asList(Motherboard.class, Processor.class, Memory.class, Casing.class));
-
-        // Removing all presentclasses from requiredclasses means that any classes that were required and not
-        // in the parts list would still be there, thus requiredclasses would not be empty
         requiredClasses.removeAll(presentClasses);
-
-        // If requiredclasses is empty all required parts would be there
         return requiredClasses.isEmpty();
     }
 }
